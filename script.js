d69b356248cb421b7103a5206e58c3dc05c010e8
Sergio-Romero12 <100429116@alumnos.uc3m.es>
2023-09-01 19:25:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+/* item_navbar_left = document.querySelectorAll(".item");
+
+item_navbar_left.forEach((e) => {
+    e.addEventListener('mouseover', () => {
+        // Change the button's background color
+        e.style.color = 'green';
+    });
+});
+
+item_navbar_left.forEach((e) => {
+    e.addEventListener('mouseout', () => {
+        // Change the button's background color
+        e.style.color = '';
+    });
+}); */
